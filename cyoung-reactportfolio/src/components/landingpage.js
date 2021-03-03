ef39daf4dc13825a8ee0dd4e2623f1efbf5db1f1
Cailin Young <cailinfayyoung@gmail.com>
2021-03-03 00:17:11 -0700

    adding css to landing page

--- cyoung-reactportfolio/src/components/landingpage.js
+++ cyoung-reactportfolio/src/components/landingpage.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl'
 
 class Landing extends Component{
     render (){
         return (
-            <div>
-                <h1> Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className='landingGrid'>
+                    <Cell col={12}>
+                        <img 
+                            src='./public/images/cailinavatar'
+                            alt="cailin young avatar" 
+                            className="avatar-image"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr/>
+
+                            <p>
+                                HTML | CSS | JAVASCRIPT | MYSQL | NODE JS | REACT | MONGODB | EXPRESS
+                            </p>
+                            <div className="social-links">
+                                {/* linkdin */}
+                                <a href="http:/google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                </a>
+                                {/* github */}
+                                <a href="http:/google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true"/>
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
