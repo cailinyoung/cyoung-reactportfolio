b5c36b6682dc1a24fdb29d032fed49554c0ea5d7
Cailin Young <cailinfayyoung@gmail.com>
2021-03-05 21:16:39 -0700

    fixing portfolio links

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,8 +11,8 @@ class Projects extends Component {
                             <CardTitle className="card-title" style={{backgroundImage:"url('assets/images/passwordgen.png')"}}></CardTitle>
                             <CardText>Password Generator</CardText>
                             <CardActions border>
-                                <Button colored href="https://github.com/cailinyoung/c-young-password-generator">GitHub</Button>
-                                <Button colored href="https://cailinyoung.github.io/c-young-password-generator/">LiveSite</Button>
+                                <Button colored href="https://github.com/cailinyoung/c-young-password-generator" target="_blank">GitHub</Button>
+                                <Button colored href="https://cailinyoung.github.io/c-young-password-generator/" target="_blank">Live Site</Button>
                             </CardActions>
                             <CardMenu style={{color:'#fff'}}>
                                 <IconButton name="share"/>
@@ -22,8 +22,8 @@ class Projects extends Component {
                             <CardTitle className="card-title" style={{backgroundImage:"url('assets/images/dayplanner.png')"}}></CardTitle>
                             <CardText>Day Planner</CardText>
                             <CardActions border>
-                                <Button colored href="https://github.com/cailinyoung/cyoung-dayplanner">GitHub</Button>
-                                <Button colored href="https://cailinyoung.github.io/cyoung-dayplanner/">LiveSite</Button>
+                                <Button colored href="https://github.com/cailinyoung/cyoung-dayplanner" target="_blank">GitHub</Button>
+                                <Button colored href="https://cailinyoung.github.io/cyoung-dayplanner/" target="_blank">Live Site</Button>
                             </CardActions>
                             <CardMenu style={{color:'#fff'}}>
                                 <IconButton name="share"/>
@@ -33,8 +33,8 @@ class Projects extends Component {
                             <CardTitle className="card-title" style={{backgroundImage:"url('assets/images/projectone.png')"}}></CardTitle>
                             <CardText>Project 1 - NPOCA</CardText>
                             <CardActions border>
-                                <Button colored href="https://github.com/chloewils/weatherNationalParks">GitHub</Button>
-                                <Button colored href="https://chloewils.github.io/weatherNationalParks">LiveSite</Button>
+                                <Button colored href="https://github.com/chloewils/weatherNationalParks" target="_blank">GitHub</Button>
+                                <Button colored href="https://chloewils.github.io/weatherNationalParks" target="_blank">Live Site</Button>
                             </CardActions>
                             <CardMenu style={{color:'#fff'}}>
                                 <IconButton name="share"/>
@@ -46,8 +46,8 @@ class Projects extends Component {
                             <CardTitle className="card-title" style={{backgroundImage:"url('assets/images/fitnesstracker.png')"}}></CardTitle>
                             <CardText>Fitness Tracker</CardText>
                             <CardActions border>
-                                <Button colored href="https://github.com/cailinyoung/c-young-hw17">GitHub</Button>
-                                <Button colored href=" https://c-young-hw17.herokuapp.com/?id=603af31eca7fde0015042cdd"></Button>
+                                <Button colored href="https://github.com/cailinyoung/c-young-hw17" target="_blank">GitHub</Button>
+                                <Button colored href=" https://c-young-hw17.herokuapp.com/?id=603af31eca7fde0015042cdd" target="_blank">Live Site</Button>
                             </CardActions>
                             <CardMenu style={{color:'#fff'}}>
                                 <IconButton name="share"/>
@@ -57,8 +57,8 @@ class Projects extends Component {
                             <CardTitle className="card-title" style={{backgroundImage:"url('assets/images/notetaker.png')"}}></CardTitle>
                             <CardText>Note Taker</CardText>
                             <CardActions border>
-                                <Button colored href="https://github.com/cailinyoung/cyoung-notetaker">GitHub</Button>
-                                <Button colored href="https://cyoung-notetaker.herokuapp.com/">LiveSite</Button>
+                                <Button colored href="https://github.com/cailinyoung/cyoung-notetaker" target="_blank">GitHub</Button>
+                                <Button colored href="https://cyoung-notetaker.herokuapp.com/"target="_blank">Live Site</Button>
                             </CardActions>
                             <CardMenu style={{color:'#fff'}}>
                                 <IconButton name="share"/>
@@ -68,8 +68,8 @@ class Projects extends Component {
                             <CardTitle className="card-title" style={{backgroundImage:"url('assets/images/nodejs.png')"}}></CardTitle>
                             <CardText>NodeJS</CardText>
                             <CardActions border>
-                                <Button colored href="https://github.com/cailinyoung/cyoung-nodeJS">GitHub</Button>
-                                <Button colored href="https://youtu.be/pAiTAv-3F6U">Demo Video</Button>
+                                <Button colored href="https://github.com/cailinyoung/cyoung-nodeJS" target="_blank">GitHub</Button>
+                                <Button colored href="https://youtu.be/pAiTAv-3F6U" target="_blank">Demo Video</Button>
                             </CardActions>
                             <CardMenu style={{color:'#fff'}}>
                                 <IconButton name="share"/>
